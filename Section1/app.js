04f1b47bd4b873628e470780801e3073e23824f4
Fernando Leal <lealp.fernando@gmail.com>
2022-08-25 18:20:06 -0700

    End of CDN Section

--- Section1/app.js
+++ Section1/app.js
@@ -3,12 +3,18 @@ console.log('Hello Script');
 const app = Vue.createApp({
   data() {
     return {
+      url: 'https://google.com',
       showBooks: true,
       title: 'The Final Empire',
       author: 'Brandon Sanderson',
       age: 45,
       x: 0,
       y: 0,
+      books: [
+        { title: 'Book 1', author: 'Fernando', isFav: true },
+        { title: 'Book 2', author: 'Emiliano', isFav: false },
+        { title: 'Book 3', author: 'Gustavo', isFav: true },
+      ],
     };
   },
   methods: {
